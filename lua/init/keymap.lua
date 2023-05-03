47c80ff5788bc78b97feb0fd29576f1e76a0ed68
ulysse <ulysse.touze_buord@yahoo.com>
2023-05-03 10:21:37 +0200

    Adding binding for indenting

--- lua/init/keymap.lua
+++ lua/init/keymap.lua
@@ -8,8 +8,17 @@ vim.keymap.set("n", "<Space>", "<Nop>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "gg", "ggzz")
+vim.keymap.set("n", "G", "Gzz")
+
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set("n", "<C-J>", ":bnext<CR>")
 vim.keymap.set("n", "<C-K>", ":bprevious<CR>")
