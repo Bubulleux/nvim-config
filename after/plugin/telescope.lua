dca780fe938d28a5ce9aaaacecc6a92269e4374e
Bubulle <bubulle86@yahoo.com>
2023-06-29 14:10:55 +0200

    Adding live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
+vim.keymap.set("n", "<leader>pa", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<F1>", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>c", builtin.git_commits, {})
